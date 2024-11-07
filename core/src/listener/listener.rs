22e2dcb41748c8287bd70aa115178e1f0e36371d
MMADUs <m.nizwa01@gmail.com>
2024-11-07 13:20:00 +0700

    feat: added dynamic stream process for connection handler

--- core/src/listener/listener.rs
+++ core/src/listener/listener.rs
@@ -1,13 +1,29 @@
 use tokio::{
-    net::{ TcpListener, UnixListener },
-    io::{ self },
+    io::{self, AsyncRead, AsyncWrite},
+    net::{TcpListener, TcpStream, UnixListener, UnixStream},
 };
 
+// Generic trait for our streams
+trait StreamExt: AsyncRead + AsyncWrite + Unpin + Send {
+    fn split(self) -> (io::ReadHalf<Self>, io::WriteHalf<Self>)
+    where
+        Self: Sized,
+    {
+        io::split(self)
+    }
+}
+
+// Blanket implementation for TcpStream and UnixStream
+impl StreamExt for TcpStream {}
+impl StreamExt for UnixStream {}
+
+#[derive(Debug)]
 enum ListenerType {
     TCP,
     UDS,
 }
 
+#[derive(Debug)]
 pub struct Listener {
     listener_type: ListenerType,
     address: String,
@@ -27,7 +43,7 @@ impl Listener {
             address: addr.to_string(),
         }
     }
-    
+
     pub async fn listen(&self) -> io::Result<()> {
         match self.listener_type {
             ListenerType::TCP => self.tcp_listener().await,
@@ -46,10 +62,13 @@ impl Listener {
                 // shutdown signal here to break loop
             };
             match new_io {
-                Ok((_downstream, _addr)) => {
+                Ok((downstream, _addr)) => {
                     println!("new tcp connection accepted");
                     tokio::spawn(async move {
                         // handle here
+                        if let Err(e) = self.handle_connection(downstream).await {
+                            println!("tcp handler error: {}", e);
+                        }
                     });
                 }
                 Err(e) => {
@@ -70,10 +89,13 @@ impl Listener {
                 // shutdown signal here to break loop
             };
             match new_io {
-                Ok((_downstream, _addr)) => {
+                Ok((downstream, _addr)) => {
                     println!("new uds connection accepted");
                     tokio::spawn(async move {
                         // handle here
+                        if let Err(e) = self.handle_connection(downstream).await {
+                            println!("uds handle error: {}", e);
+                        }
                     });
                 }
                 Err(e) => {
@@ -81,5 +103,28 @@ impl Listener {
                 }
             };
         }
+    }
+
+    async fn handle_connection<S>(&self, downstream: S) -> io::Result<()>
+    where
+        S: StreamExt + 'static,
+    {
+        // let upstream = TcpStream::connect(&upstream_addr).await?;
+        //
+        // let (mut downstream_read, mut downstream_write) = downstream.split();
+        // let (mut upstream_read, mut upstream_write) = upstream.split();
+        //
+        // let client_to_upstream = async {
+        //     io::copy(&mut downstream_read, &mut upstream_write).await?;
+        //     upstream_write.shutdown().await
+        // };
+        //
+        // let upstream_to_client = async {
+        //     io::copy(&mut upstream_read, &mut downstream_write).await?;
+        //     downstream_write.shutdown().await
+        // };
+        //
+        // tokio::try_join!(client_to_upstream, upstream_to_client)?;
+        Ok(())
     }
 }
